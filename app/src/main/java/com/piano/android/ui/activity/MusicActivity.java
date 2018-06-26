f0d6cba86bd6748bf003d481f61d4a27a90ace2e
kaka <125921384@qq.com>
2018-06-26 12:52:28 +0800

    no message

--- app/src/main/java/com/piano/android/ui/activity/MusicActivity.java
+++ app/src/main/java/com/piano/android/ui/activity/MusicActivity.java
@@ -7,6 +7,8 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 
 import com.chico.common.CacheUtils;
@@ -35,30 +37,36 @@ import jp.kshoji.driver.midi.device.MidiOutputDevice;
 *
 *
 * */
-public class MusicActivity extends BaseMidiActivity {
+public class MusicActivity extends BaseMidiActivity{
 
-    //默认伴弹状态，  0伴弹，播放   1跟弹，
-    int state = 0;
+    // 1 左手 0 右手
+    int handCode = 2;
+    // 0 伴奏  1跟奏
+    int typeCode = 0;
 
+    //---------跟弹 伴弹
     Button btn_play = null;
     Button btn_pause = null;
-
-    Button btn_banzou = null;
-    Button btn_gentan = null;
-
     Button btn_startAB = null;
     Button btn_endAB = null;
-
-    Button btn_lefthand = null;
-    Button btn_righthand = null;
-
     Button[] mBtns = null;
 
+    //---------左手 右手
+    CheckBox cb_bantan = null;
+    CheckBox cb_gentan = null;
+    CheckBox cb_left = null;
+    CheckBox cb_right = null;
+    CheckBox cb_both = null;
+    CheckBox[] boxType = new CheckBox[2];
+    CheckBox[] handType = new CheckBox[3];
+
+    //--------播放参数
     String fileName = "";
     String filePath = "";
     String fileDir = "";
     String fileUrl = "";
 
+    //---------- 渲染
     MusicScorePlayer player;
     MidiOutputDevice midiO;
     Iled led;
@@ -82,17 +90,34 @@ public class MusicActivity extends BaseMidiActivity {
     public void initView(){
 
         ViewGroup vc = (ViewGroup) this.getLayoutInflater().inflate(R.layout.activity_note, null);
+
+        cb_bantan = (CheckBox) vc.findViewById(R.id.cb_bantan);
+        cb_gentan = (CheckBox) vc.findViewById(R.id.cb_gentan);
+        cb_left = (CheckBox) vc.findViewById(R.id.cb_left);
+        cb_right = (CheckBox) vc.findViewById(R.id.cb_right);
+        cb_both = (CheckBox) vc.findViewById(R.id.cb_both);
+
+        CheckLisner1 c1 = new CheckLisner1();
+        CheckLisner2 c2 = new CheckLisner2();
+        cb_bantan.setOnCheckedChangeListener(c1);
+        cb_gentan.setOnCheckedChangeListener(c1);
+        cb_left.setOnCheckedChangeListener(c2);
+        cb_right.setOnCheckedChangeListener(c2);
+        cb_both.setOnCheckedChangeListener(c2);
+
+        boxType[0] = cb_bantan;
+        boxType[1] = cb_gentan;
+        handType[0] = cb_left;
+        handType[1] = cb_right;
+        handType[2] = cb_both;
+
         btn_pause = (Button) vc.findViewById(R.id.button1);
         btn_play = (Button) vc.findViewById(R.id.button2);
 
         btn_startAB = (Button) vc.findViewById(R.id.button3);
         btn_endAB = (Button) vc.findViewById(R.id.button4);
 
-        btn_banzou = (Button) vc.findViewById(R.id.button5);
-        btn_gentan = (Button) vc.findViewById(R.id.button6);
 
-        btn_lefthand = (Button) vc.findViewById(R.id.button7);
-        btn_righthand = (Button) vc.findViewById(R.id.button8);
         btn_pause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -117,38 +142,10 @@ public class MusicActivity extends BaseMidiActivity {
                 mEndAB();
             }
         });
-        //伴弹
-        btn_banzou.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setBanZou(0);
-            }
-        });
-        //跟弹
-        btn_gentan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setGenTan(0);
-            }
-        });
-        btn_lefthand.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                player.setHand(1);
-            }
-        });
-        btn_righthand.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                player.setHand(0);
-            }
-        });
 
         mFrameLayout.addView(vc, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 150, Gravity.BOTTOM));
 
-        mBtns = new Button[]{btn_play,btn_pause,
-                btn_banzou,btn_gentan,
-                btn_lefthand,btn_righthand,btn_startAB,btn_endAB};
+        mBtns = new Button[]{btn_play,btn_pause};
 
 
         showLoading();
@@ -196,35 +193,21 @@ public class MusicActivity extends BaseMidiActivity {
             player.pause();
             player = null;
         }
-//        try {
-//            Thread.sleep(2000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-    }
-
-    //设置伴奏模式
-    public void setBanZou(int hand){
-        if(null!=player){
-            state = 0;
-            player.setHand(2);
-            player.setMode(0);
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-    }
 
-    //设置跟弹模式
-    public void setGenTan(int hand){
-        if(null!=player){
-            state = 1;
-            player.setHand(2);
-            player.setMode(1);
-        }
+        this.finish();
+
     }
 
     //开始模式
     public void mPlay(){
         if(null!=player){
+            player.setHand(handCode);
+            player.setMode(typeCode);
             player.play();
             setBtnEnable(btn_pause);
         }
@@ -247,8 +230,21 @@ public class MusicActivity extends BaseMidiActivity {
 
     public void mEndAB(){
         if(null!=player){
-            player.unsetAB();
-            setBtnEnable(null);
+//            player.unsetAB();
+//            setBtnEnable(null);
+
+            if(player!=null){
+                player.pause();
+                player = null;
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            this.finish();
+
         }
     }
 
@@ -274,9 +270,12 @@ public class MusicActivity extends BaseMidiActivity {
                     return;
                 }
 
-                if(state==0) {
+                if(typeCode==0) {
                     //midi发声
-                    midiO.sendMidiNoteOn(0x00, (byte) 0x00, note, 0x32);
+                    if(ison==1) {
+                        midiO.sendMidiNoteOn(0x00, (byte) 0x00, note, 0x32);
+                    }
+
                     if(ison==1){
                         led.setLedStatus(true,true,note);
                     }else{
@@ -284,7 +283,7 @@ public class MusicActivity extends BaseMidiActivity {
                     }
 
 
-                }else if(state==1){
+                }else if(typeCode==1){
                     //亮灯
                     if(ison==1){
                         led.setLedStatus(true,true,note);
@@ -292,13 +291,6 @@ public class MusicActivity extends BaseMidiActivity {
                         led.setLedStatus(true,false,note);
                     }
                 }
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        btn_righthand.setText(note + "-" + state );
-                    }
-                });
 
 
             }
@@ -337,25 +329,17 @@ public class MusicActivity extends BaseMidiActivity {
     @Override
     public void onMidiNoteOn(@NonNull MidiInputDevice sender, int cable, int channel, final int note, int velocity) {
         super.onMidiNoteOn(sender, cable, channel, note, velocity);
+
         player.sendMidi(0,note,50);
-//        runOnUiThread(new Runnable() {
-//            @Override
-//            public void run() {
-//                btn_lefthand.setText("" + note);
-//            }
-//        });
+
     }
 
     @Override
     public void onMidiNoteOff(@NonNull MidiInputDevice sender, int cable, int channel, final int note, int velocity) {
         super.onMidiNoteOff(sender, cable, channel, note, velocity);
+
         player.sendMidi(0,note,0);
-//        runOnUiThread(new Runnable() {
-//            @Override
-//            public void run() {
-//                btn_lefthand.setText("" + note);
-//            }
-//        });
+
     }
 
     private void setBtnEnable(Button... btns){
@@ -382,6 +366,8 @@ public class MusicActivity extends BaseMidiActivity {
 
         btn_startAB.setEnabled(false);
         btn_endAB.setEnabled(false);
+
+        btn_endAB.setEnabled(true);
     }
 
 
@@ -399,6 +385,49 @@ public class MusicActivity extends BaseMidiActivity {
             loadingDialog.dismiss();
         }
         loadingDialog = null;
+    }
+
+    class CheckLisner1 implements CompoundButton.OnCheckedChangeListener{
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if(b) {
+                for (int i = 0; i < boxType.length; i++) {
+                    //不等于当前选中的就变成false
+                    if (boxType[i].getText().toString().equals(compoundButton.getText().toString())) {
+                        if(i==0){
+                            typeCode = 0;
+                        }else{
+                            typeCode = 1;
+                        }
+                    }else{
+                        boxType[i].setChecked(false);
+                    }
+                }
+            }
+        }
+    }
+
+    class CheckLisner2 implements CompoundButton.OnCheckedChangeListener{
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if(b) {
+                for (int i = 0; i < handType.length; i++) {
+                    //不等于当前选中的就变成false
+                    if (handType[i].getText().toString().equals(compoundButton.getText().toString())) {
+                        if(i==0){
+                            handCode = 1;
+                        }else if(i==1){
+                            handCode = 0;
+                        }else{
+                            handCode = 2;
+                        }
+                    }else{
+                        handType[i].setChecked(false);
+                    }
+                }
+            }
+
+        }
     }
 
 
